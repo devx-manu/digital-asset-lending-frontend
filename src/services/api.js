f6ec6851f4b24a1e94e41a3cdd7f75f12b47c0c8
Manu SH <devx.manu@gmail.com>
2026-05-08 20:47:06 +0530

    Change API base URL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://digital-asset-lending-backend.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
